fb824dbf5051c6567c57b3c66c5b4720dee06326
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-01-16 11:15:35 -0800

    tests added

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -2,6 +2,24 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CharacterFrequencyCounterTest {
+public class CharacterFrequencyCounterTest {    
     
+    // Tests are always public void
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+        // Act
+        int actualCount = counter.getFrequency('l');
+        // Assert
+        assertEquals(3, actualCount);
+    }
+
+    @Test
+    public void testNonOccuringCharacterPercentageIsZero() {
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+        double actualPercentage = counter.getRelativePercentage('z');
+        assertEquals(0.0, actualPercentage, 0.0001);
+    }
+
 }
